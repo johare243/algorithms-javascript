71116377bf750e52917ff2e3e8be924248c95c4f
johare243 <johare243@gmail.com>
2018-05-11 17:35:10 -0400

    add github.com/StephenGrider/algocasts folder

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,38 +1,29 @@
-//For numbers up to n if number is divisible by 3 print fizz, divisible by 5 print buzz
-//if divisible by both print fizzbuzz
-//html below for testing
+// --- Directions
+// Write a program that console logs the numbers
+// from 1 to n. But for multiples of three print
+// “fizz” instead of the number and for the multiples
+// of five print “buzz”. For numbers which are multiples
+// of both three and five print “fizzbuzz”.
+// --- Example
+//   fizzBuzz(5);
+//   1
+//   2
+//   fizz
+//   4
+//   buzz
 
-function fizzbuzz(n) {
-	var n = document.getElementById('n').value;
-  var result = ''; 
-  for (var i = 0; i < n; i++) {
-		if (i == 0) {
-    	result += '0<br>';
-      continue;
-    }
-    if (i % 3 == 0) {
-      if (i % 5 == 0) {  // divisible by 3 and 5
-        result += "fizzbuzz<br>";
-        continue;
-      } else {           // divisible by 3 only
-        result += "fizz<br>";
-        continue;
-      } 
-    } 
-
-    if (i % 5 == 0 && i % 3 != 0) { //divisible by 5 only
-    result += "buzz<br>";
-    continue;
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
     } else {
-      result += i + "<br>";
-    } 
-  }   
-  document.getElementById('output').innerHTML = result;
+      console.log(i);
+    }
+  }
 }
 
-//HTML for testing
-//
-//<input type='text' id='n'>
-//<input type='button' id='submit' value='submit' onclick='fizzbuzz(n)'>
-//<div id='output'>
-//</div>
+module.exports = fizzBuzz;
